bf6ac23e300ebd9846794b26b53d472234d7ba92
Fabricio <fegrijalva2501@hotmail.com>
2023-05-17 11:57:00 -0500

    dominios de oferentes

---
+++ grails-app/domain/janus/ObraOferente.groovy
@@ -0,0 +1,32 @@
+package janus
+
+import audita.Auditable
+import seguridad.Persona
+
+class ObraOferente implements Auditable{
+
+    Obra obra
+    Persona persona
+    Date fecha
+    static auditable = true
+
+    static mapping = {
+        table 'obof'
+        cache usage: 'read-write', include: 'non-lazy'
+        id column: 'obof__id'
+        id generator: 'identity'
+        version false
+        columns {
+            id column: 'obof__id'
+            obra column: 'obra__id'
+            persona column: 'prsn__id'
+            fecha column: 'oboffcha'
+        }
+    }
+
+    static constraints = {
+        obra(blank: false, nullable: false)
+        persona(blank: false, nullable: false)
+        fecha(blank: false, nullable: false)
+    }
+}
