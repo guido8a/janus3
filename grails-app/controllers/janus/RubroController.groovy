4b3c166f383b6ed3aea529b6b545e3dfdf1154fe
guido8a <guido8a@gmail.com>
2023-09-08 10:38:37 -0500

    variables precios

--- grails-app/controllers/janus/RubroController.groovy
+++ grails-app/controllers/janus/RubroController.groovy
@@ -656,7 +656,8 @@ class RubroController {
     def getPreciosTransporte() {
         println "get precios fecha: "+params.fecha
         def lugar = Lugar.get(params.ciudad)
-        def fecha = new Date().parse("dd-MM-yyyy", params.fecha)
+        //def fecha = new Date().parse("dd-MM-yyyy", params.fecha)
+        def fecha = new Date().parse("yyyy-MM-dd", params.fecha)
         def tipo = params.tipo
         def items = []
         def parts = params.ids.split("#")
